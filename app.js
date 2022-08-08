b35029a3538fdbbad0d19f3bb0adc08deb8fe2b0
Soobratty Anas <soobratty.as@gmail.com>
2022-08-08 15:53:50 +0400

    player score

--- app.js
+++ app.js
@@ -7,26 +7,43 @@ const app = Vue.createApp({
         return {
             playerHealth: 100,
             monsterHealth: 100,
-            currentRound: 0
+            currentRound: 0,
+            scored: 0,
+            monsterScore: 0,
+            playerScore: 0,
+            bolean: false,
+            damage: [],
+            damageGotten: []
         }
     },
 
     methods: {
+        damageDealt(value) {
+            this.damage.push(`You did ${value} damage`)
+        },
+
+        damageGot(value) {
+            this.damageGotten.push(`recieved ${value} damage`)
+        },
+
         launchAttack() {
             this.currentRound++;
             const damage = getRandValue(5, 12);
             this.monsterHealth -= damage;
             this.monsterAttack();
+            this.damageDealt(damage);
         },
         monsterAttack() {
             const damage = getRandValue(8, 15);
             this.playerHealth -= damage;
+            this.damageGot(damage);
         },
         specialAttackMonster() {
             this.currentRound++;
             const damage = getRandValue(10, 25);
             this.monsterHealth -= damage;
             this.monsterAttack();
+            this.damageDealt(damage);
         },
         healPlayer() {
             this.currentRound++;
@@ -36,19 +53,40 @@ const app = Vue.createApp({
             } else {
                 this.playerHealth += heal;
             }
-
             this.monsterAttack();
+        },
+        newGame() {
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.currentRound = 0;
+            this.scored = 0;
+            this.damage = [];
+        },
+
+        surrender() {
+            this.scored = 2;
+            this.monsterScore += 1;
+        },
+
+        revealBattleLog() {
+            this.bolean = !this.bolean;
         }
     },
 
     computed: {
         monsterStyle() {
+            if (this.monsterHealth < 0) {
+                return { width: '0%' }
+            }
             return {
                 width: this.monsterHealth + '%'
             }
         },
 
         playerStyle() {
+            if (this.playerHealth < 0) {
+                return { width: '0%' }
+            }
             return {
                 width: this.playerHealth + '%'
             }
@@ -56,6 +94,26 @@ const app = Vue.createApp({
 
         enableSpecialattack() {
             return this.currentRound % 3 !== 0
+        }
+    },
+
+    watch: {
+        playerHealth(value) {
+            if (value < 1 && this.monsterHealth <= 0) {
+                this.scored = 1;
+            } else if (value <= 0) {
+                this.scored = 2;
+                this.playerScore += 1;
+            }
+        },
+
+        monsterHealth(value) {
+            if (value < 1 && this.playerHealth <= 0) {
+                this.scored = 1;
+            } else if (value <= 0) {
+                this.scored = 3;
+                this.monsterScore += 1;
+            }
         }
     }
 });
